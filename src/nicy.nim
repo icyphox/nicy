421cac1e7f9c73666bddd5c38c6677ebab32d3b8
Juan Carlos <juancarlospaco@gmail.com>
2019-10-30 15:24:39 -0300

    Rename echo to echoc

--- src/nicy.nim
+++ src/nicy.nim
@@ -1,19 +1,15 @@
-import 
-  nicypkg/functions,
-  strformat
-
-export
-  functions
+import strformat, times, os, osproc, nicypkg/functions
+export functions
 
 when isMainModule:
   let
-    prompt = color("› ", "magenta")
+    prompt = returnCondition(ok = "👍", ng = "👎") & " "
     nl = "\n"
     gitBranch = color(gitBranch(), "yellow")
     cwd = color(tilde(getCwd()), "cyan")
     dirty = color("×", "red")
     clean = color("•", "green")
-  let git = gitBranch & gitStatus(dirty, clean)
-
-  # the prompt
-  echo fmt"{nl}{virtualenv()}{cwd}{git}{nl}{prompt}"
+    git = gitBranch & gitStatus(dirty, clean)
+    ni = execCmdEx("nim --version").output[21 .. 25]
+    py = execCmdEx("python --version").output[7 .. ^4]
+  echoc cstring(fmt"{user()}@x {virtualenv()}{cwd} {getClockStr()[0..^4]} Py{py} Nim{ni} {git}{nl}{prompt}")
