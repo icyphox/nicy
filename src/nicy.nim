dc961f5808b84bbe224fd194fed97145dea825be
Juan Carlos <juancarlospaco@gmail.com>
2019-10-30 15:28:01 -0300

    Revert "Rename echo to echoc"
    This reverts commit 421cac1e7f9c73666bddd5c38c6677ebab32d3b8.

--- src/nicy.nim
+++ src/nicy.nim
@@ -1,15 +1,19 @@
-import strformat, times, os, osproc, nicypkg/functions
-export functions
+import 
+  nicypkg/functions,
+  strformat
+
+export
+  functions
 
 when isMainModule:
   let
-    prompt = returnCondition(ok = "👍", ng = "👎") & " "
+    prompt = color("› ", "magenta")
     nl = "\n"
     gitBranch = color(gitBranch(), "yellow")
     cwd = color(tilde(getCwd()), "cyan")
     dirty = color("×", "red")
     clean = color("•", "green")
-    git = gitBranch & gitStatus(dirty, clean)
-    ni = execCmdEx("nim --version").output[21 .. 25]
-    py = execCmdEx("python --version").output[7 .. ^4]
-  echoc cstring(fmt"{user()}@x {virtualenv()}{cwd} {getClockStr()[0..^4]} Py{py} Nim{ni} {git}{nl}{prompt}")
+  let git = gitBranch & gitStatus(dirty, clean)
+
+  # the prompt
+  echo fmt"{nl}{virtualenv()}{cwd}{git}{nl}{prompt}"
